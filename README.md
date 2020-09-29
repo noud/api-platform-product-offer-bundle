3c4ea770ff67cc5624204ef1fac14cfa0553ae81
Noud de Brouwer <noud4@home.nl>
2020-09-29 21:53:13 +0200

    filter

---
+++ README.md
@@ -0,0 +1,2 @@
+# Products and Offers
+## [Postman collection](http://github.com/noud/api-platform-product-offer-bundle/blob/master/doc/API-Platform-offer-product.postman_collection.json) with filters
--- src/Entity/Offer.php
+++ src/Entity/Offer.php
@@ -4,6 +4,13 @@ namespace Noud\ProductOfferBundle\Entity;
 
 use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\ExistsFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\NumericFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use Doctrine\ORM\Mapping as ORM;
 use Noud\UtilBundle\Filter\RegexpFilter;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -12,7 +19,14 @@ use Symfony\Component\Validator\Constraints as Assert;
  * An offer from my shop - this description will be automatically extracted from the PHPDoc to document the API.
  *
  * @ApiResource(iri="http://schema.org/Offer")
- * @ApiFilter(RegexpFilter::class)
+ * @ ApiFilter(RegexpFilter::class)
+ * @ ApiFilter(DateFilter::class, properties={"createdAt": DateFilter::EXCLUDE_NULL})
+ * @ApiFilter(SearchFilter::class, properties={"id": "exact", "price": "exact", "description": "partial", "product": "exact"})
+ * @ ApiFilter(BooleanFilter::class, properties={"isAvailableGenericallyInMyCountry"})
+ * @ ApiFilter(NumericFilter::class, properties={"sold"})
+ * @ApiFilter(RangeFilter::class, properties={"price"})
+ * @ ApiFilter(ExistsFilter::class, properties={"transportFees"})
+ * @ApiFilter(OrderFilter::class, properties={"id", "name"}, arguments={"orderParameterName"="order"})
  * @ORM\Entity
  */
 class Offer
@@ -38,6 +52,18 @@ class Offer
     public $price;
 
     /**
+     * @ORM\Column(type="bool")
+     * @Assert\Type(type="bool")
+     */
+    public $availableGenericallyInMyCountry = true;
+
+    /**
+     * @ORM\Column(type="bool", nullable=true)
+     * @Assert\Type(type="bool")
+     */
+    public $sold = false;
+
+    /**
      * @ORM\ManyToOne(targetEntity="Product", inversedBy="offers")
      */
     public $product;
@@ -45,5 +71,10 @@ class Offer
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function isAvailableGenericallyInMyCountry(): bool
+    {
+        return $this->availableGenericallyInMyCountry;
     }
 }
